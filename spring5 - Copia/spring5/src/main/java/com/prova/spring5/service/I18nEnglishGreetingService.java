d8e12145646ac283d0ec52d0c43b617ca9d0008b
giorgio modeo <75391952+giorgio-modeo@users.noreply.github.com>
2024-01-30 16:06:58 +0100

    pojo + multi module first

--- spring5 - Copia/spring5/src/main/java/com/prova/spring5/service/I18nEnglishGreetingService.java
+++ spring5 - Copia/spring5/src/main/java/com/prova/spring5/service/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.prova.spring5.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
