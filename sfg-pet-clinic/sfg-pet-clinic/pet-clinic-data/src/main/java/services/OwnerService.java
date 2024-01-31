097e8740b3ff7a8b9a8c166e485c1cb6c8999f5f
giorgio modeo <75391952+giorgio-modeo@users.noreply.github.com>
2024-01-31 14:29:40 +0100

    top

---
+++ sfg-pet-clinic/sfg-pet-clinic/pet-clinic-data/src/main/java/services/OwnerService.java
@@ -0,0 +1,9 @@
+package services;
+
+import model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService extends CrudService<Owner, Long>{
+    Owner findByLastName(String lastName);
+}
---
+++ spring5-jokes/src/test/java/com/weplus/spring5jokes/Spring5JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weplus.spring5jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
