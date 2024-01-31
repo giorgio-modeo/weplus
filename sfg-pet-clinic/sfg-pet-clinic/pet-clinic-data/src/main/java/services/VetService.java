31543225718f0233562fef33b7998fc9a51759b6
giorgio modeo <75391952+giorgio-modeo@users.noreply.github.com>
2024-01-31 12:40:42 +0100

    Impl Map based Service map

--- sfg-pet-clinic/sfg-pet-clinic/pet-clinic-data/src/main/java/services/VetService.java
+++ sfg-pet-clinic/sfg-pet-clinic/pet-clinic-data/src/main/java/services/VetService.java
@@ -4,8 +4,6 @@ import model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findByID(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
+
 }
